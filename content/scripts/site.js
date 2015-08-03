8881115257464887a084fb7d19dbdbbcb75a710a
tracer-sec <tracer@hush.com>
2015-08-03 22:07:17 +0100

    Added next meet date to the top of the page. Split some scripts

--- content/scripts/site.js
+++ content/scripts/site.js
@@ -1,16 +1,11 @@
 (function () {
-    $.getJSON('/content/data/events.js', function (data) {
-        var markup = '';
-        var dateNow = new Date();
-        dateNow.setHours(0, 0, 0, 0);
-        data.events.sort(function (a, b) { return a.start_date.localeCompare(b.start_date); });
-        data.events = data.events.filter(function (e) { return new Date(e.start_date) >= dateNow; });
-        for (var i = 0; i < Math.min(5, data.events.length); i ++) {
-            var d = data.events[i];
-            var date = new Date(d.start_date);
-            markup += '<li>' + date.toDateString() + ' - <a href="' + d.url + '">' + d.name + '</a> - <em>' + d.location + '</em></li>';
-        }
-        markup = '<ul>' + markup + '</ul>';
-        $('#events').html(markup);
+    var nextMeetDate = new Date();
+    nextMeetDate.setHours(18, 30, 0, 0);
+    while (nextMeetDate.getDay() != 5)
+        nextMeetDate.setDate(nextMeetDate.getDate() + 1);
+    while (nextMeetDate.getDate() > 7)
+        nextMeetDate.setDate(nextMeetDate.getDate() + 7);
+    $(function () {
+        $('#nextmeet span').html(nextMeetDate.toLocaleString());
     });
 }) ();
